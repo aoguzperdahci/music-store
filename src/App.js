0d2d296e4eb059f14afcbbef2abe812a0841d2e2
Eda Güneş <gunnes.eda@gmail.com>
2022-01-02 17:54:27 +0300

    Footer updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React, { Component } from "react";
 import Footer from "./Components/Footer";
 import MenuBar from "./Components/MenuBar";
 import Page from "./Components/Page";
 
-function App() {
-  return (
-<>
- <MenuBar></MenuBar>
- <Page></Page>
-<Footer></Footer>
-</>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MenuBar></MenuBar>
+        <Page></Page>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
