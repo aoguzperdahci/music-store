123349b17a639a34459748854e74a2ada8acce44
enazacun <83958411+enazacun@users.noreply.github.com>
2022-01-15 14:01:45 +0300

    Update5

--- src/Components/BCarousel.js
+++ src/Components/BCarousel.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import {Image} from 'react-bootstrap';
 import { Carousel, Col, Row } from 'react-bootstrap';
 import "../CSS/bCarausel.css";
+import ukulelesImage from "../Resources/images/ukuleles.jpg";
+import aletlerImage from "../Resources/images/aletler.jpg";
+import pianobrandsImage from "../Resources/images/pianobrands.jpg";
+import brandsImage from "../Resources/images/brandmusic.jpg";
 
 export default function BCarousel() {
     return (
         <div>
-            <Carousel className=''>
+            <Carousel className='background'>
                 <Carousel.Item>
                     <Row>
                         <Col lg={4} className='d-none d-lg-block text-center my-auto'>
-                        <h3>First Slide Label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        <h3 className='heading'>Discount On Every Piano</h3>
+                        <div>
+                        <p className='text'>Up to 15% - 25% discount on all piano brands on your first purchase from our website.
+                        Do not miss our campaign that is valid until the end of this January. Do not forget it is limited edition.
+                       
+                        </p> 
+                        </div>
+                        <Image className='img-size' src={pianobrandsImage}></Image>
                         </Col>
 
                         <Col lg={8}>
@@ -19,13 +30,62 @@ export default function BCarousel() {
                                 src="https://ichef.bbci.co.uk/news/976/cpsprodpb/425A/production/_120568961_gettyimages-1322472765.jpg"
                                 alt="First slide" />
                         </Col>
+                        
                     </Row>
 
                     <Carousel.Caption className='d-lg-none'>
                         <h3>First Slide Label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
+                    
 
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Row>
+                        <Col lg={4} className='d-none d-lg-block text-center my-auto'>
+                        <h3 className='heading'>Best Ukulele Brands Are Back In Stock</h3>
+                        <p className='text'>The most preferred models and brands all over the world are at your fingertips
+                         with the privilege of our brand. We provide free delivery to all over the country. We give at
+                          least 2 years of guarentee on our every brand of pianos. </p>
+                        </Col>
+
+                        <Col lg={8}>
+                            <img
+                                className="crsl-img"
+                                src={ukulelesImage}
+                                alt="First slide" />
+                        </Col>
+                        
+                    </Row>
+
+                    <Carousel.Caption className='d-lg-none'>
+                        <h3>First Slide Label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                    
+
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Row>
+                        <Col lg={4} className='d-none d-lg-block text-center my-auto'>
+                        <h3 className='heading'>Everything You Are Looking For</h3>
+                        <p className='text'>Many instrumental stuff are finally available again! Every little instrumental 
+                        tool you are in need is on this website. We have renewed our stocks! Check out for the newest products. </p>
+                        <Image className='img-size' src={brandsImage}></Image>
+                        </Col>
+                        <Col lg={8}>
+                            <img
+                                className="crsl-img"
+                                src={aletlerImage}
+                                alt="First slide" />
+                        </Col>
+                        
+                    </Row>
+
+                    <Carousel.Caption className='d-lg-none'>
+                        <h3>First Slide Label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
                 </Carousel.Item>
 
             </Carousel>
