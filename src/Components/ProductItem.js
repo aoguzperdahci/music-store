18479462f92ae2f6d7deda645431fd10e64aa554
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2022-01-15 14:39:16 +0300

    Revert

--- src/Components/ProductItem.js
+++ src/Components/ProductItem.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import "../CSS/productItem.css";
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            
-        </div>
+        <Card className='product-card text-center product-link' onClick={() => navigate("/products/" + product.id)}>
+            <Card.Img variant="top" className='product-img' src={product.images[0]} />
+
+            <Card.Title className='fs-5 text-start product-title'>{product.name}</Card.Title>
+            <div className='product-price-div'>
+                <Card.Text className='fs-3 product-price'>
+
+                    {product.price}$
+                </Card.Text>
+            </div>
+
+        </Card>
     )
 }
 
--- src/Pages/LocationsPage.js
+++ src/Pages/LocationsPage.js
@@ -39,7 +39,7 @@ const LocationsPage = () => {
                                 <iframe className='map mb-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1595.405577324372!2d30.71054894303724!3d36.894865731989285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14c39008a0f96b59%3A0x59f5fe6b8eb5566f!2zQW50YWx5YSBCw7x5w7xrxZ9laGlyIEJlbGVkaXllc2k!5e0!3m2!1str!2str!4v1641852045893!5m2!1str!2str" allowfullscreen="" loading="lazy"></iframe>
                                 <div className='locations-text-wrapper'>
                                     <p className='text-center'><FaMapMarkerAlt className='locations-icon' />Yüksekalan, 07310 Muratpaşa/Antalya</p>
-                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0242) 249 50 00</p>
+                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0242) 249 5000</p>
                                     <p className='text-start locations-schedule'><FaClock className='locations-icon' />10:00 - 21:00</p>
                                 </div>
                             </Tab.Pane>
@@ -47,7 +47,7 @@ const LocationsPage = () => {
                                 <iframe className='map mb-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3047.7045297147683!2d29.074000415701576!3d40.19338857711573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14ca3e6e21d84a1b%3A0xa79fcab777280b16!2zQnVyc2EgQsO8ecO8a8WfZWhpciBCZWxlZGl5ZXNp!5e0!3m2!1str!2str!4v1641852255143!5m2!1str!2str" allowfullscreen="" loading="lazy"></iframe>
                                 <div className='locations-text-wrapper'>
                                     <p className='text-center'><FaMapMarkerAlt className='locations-icon' />Zafer, Ankara Yolu Cd. No:1, 16270 Osmangazi/Bursa</p>
-                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0224) 716 15 50</p>
+                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0224) 716 1550</p>
                                     <p className='text-start locations-schedule'><FaClock className='locations-icon' />10:00 - 21:00</p>
                                 </div>
                             </Tab.Pane>
@@ -63,7 +63,7 @@ const LocationsPage = () => {
                                 <iframe className='map mb-4' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d464.67241673176807!2d27.128423079312242!3d38.419960455939275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14bbd8fbf38b6d23%3A0x7340c3d90468fb2a!2sKonak%20T%C3%BCketici%20Hakem%20Heyeti!5e0!3m2!1str!2str!4v1641853425074!5m2!1str!2str" allowfullscreen="" loading="lazy"></iframe>
                                 <div className='locations-text-wrapper'>
                                     <p className='text-center'><FaMapMarkerAlt className='locations-icon' />Konak, Cumhuriyet Blv Hükümet Konağı B Blok No:12, 35290 Konak/İzmir</p>
-                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0232) 489 72 13</p>
+                                    <p className='text-end locations-phone'><FaPhoneAlt className='locations-icon' /> (0232) 489 7213</p>
                                     <p className='text-start locations-schedule'><FaClock className='locations-icon' />10:00 - 21:00</p>
                                 </div>
                             </Tab.Pane>
