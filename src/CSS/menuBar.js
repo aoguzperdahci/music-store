a1b178ec4ba3f14d7ee3932820da2a85fbe54162
Ahmet Oğuzperdahçı <ahmetoguzperdahci@gmail.com>
2022-01-15 13:59:00 +0300

    visual update
    about us and contact page reshaped

--- src/CSS/menuBar.js
+++ src/CSS/menuBar.js
@@ -6,7 +6,7 @@ function stickyNav() {
     var navbar = document.getElementById("menu");
     var sticky = 66;
 
-  if (window.pageYOffset >= sticky) {
+  if (window.pageYOffset > sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
